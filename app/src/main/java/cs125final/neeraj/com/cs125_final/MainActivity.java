fd960d4ad16ccb24270e0990c3db82f3fe799932
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2018-12-10 23:36:35 -0600

    Add custom date implementation

--- app/src/main/java/cs125final/neeraj/com/cs125_final/MainActivity.java
+++ app/src/main/java/cs125final/neeraj/com/cs125_final/MainActivity.java
@@ -6,17 +6,19 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String date;
     String dateText;
 
     @Override
@@ -24,15 +26,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Date date = new Date();
-
         final String urlDate = new SimpleDateFormat("MM/d").format(date);
-
         final TextView txtView = findViewById(R.id.text);
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try  {
-                    System.out.println(urlDate);
                     URL url = new URL("http://numbersapi.com/" + urlDate + "/date");
                     BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
                     final String dateText = in.readLine();
@@ -55,13 +54,11 @@ public class MainActivity extends AppCompatActivity {
         final Button button = findViewById(R.id.simpleButton);
         button.setText("Give me another interesting fact about today");
         button.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View v) {
                 Thread thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         try  {
-                            System.out.println(urlDate);
                             URL url = new URL("http://numbersapi.com/" + urlDate + "/date");
                             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
                             final String dateText = in.readLine();
@@ -83,15 +80,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final Button button2 = findViewById(R.id.simpleButton2);
-        button2.setText("Enter a custom date");
-        button2.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                System.out.println("clicked");
-                Intent i = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
+        final EditText editText = findViewById(R.id.dateText);
+        editText.setText("Enter a custom date.");
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    final String inputDate = (editText.getText().toString());
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try  {
+                                URL url = new URL("http://numbersapi.com/" + inputDate + "/date");
+                                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                                final String dateText = in.readLine();
+                                in.close();
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        txtView.setText(dateText);
+                                    }
+                                });
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    thread.start();
+                    txtView.setGravity(Gravity.CENTER);
+                    txtView.setTextSize(20);
+                    handled = true;
+                }
+                return handled;
             }
         });
     }
