656a87e5ac4bab78a6fbb28cdd85f5321f0575bf
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2018-12-10 23:17:24 -0600

    Reload text instead of creating new layer

--- app/src/main/java/cs125final/neeraj/com/cs125_final/MainActivity.java
+++ app/src/main/java/cs125final/neeraj/com/cs125_final/MainActivity.java
@@ -17,29 +17,30 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String dateText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Date date = new Date();
 
-
         final String urlDate = new SimpleDateFormat("MM/d").format(date);
 
-        final TextView txtView = (TextView) findViewById(R.id.text);
+        final TextView txtView = findViewById(R.id.text);
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try  {
+                    System.out.println(urlDate);
                     URL url = new URL("http://numbersapi.com/" + urlDate + "/date");
                     BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-                    final String str = in.readLine();
-                    System.out.println(str);
+                    final String dateText = in.readLine();
                     in.close();
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            txtView.setText(str);
+                            txtView.setText(dateText);
                         }
                     });
                 } catch (Exception e) {
@@ -50,29 +51,48 @@ public class MainActivity extends AppCompatActivity {
         thread.start();
         txtView.setGravity(Gravity.CENTER);
         txtView.setTextSize(20);
-        //setContentView(txtView);
 
-        final Button button = (Button) findViewById(R.id.simpleButton);
+        final Button button = findViewById(R.id.simpleButton);
         button.setText("Give me another interesting fact about today");
         button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                Log.d("tag", "button was clicked");
-                Intent i = new Intent(MainActivity.this, MainActivity.class);
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try  {
+                            System.out.println(urlDate);
+                            URL url = new URL("http://numbersapi.com/" + urlDate + "/date");
+                            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                            final String dateText = in.readLine();
+                            in.close();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    txtView.setText(dateText);
+                                }
+                            });
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.start();
+                txtView.setGravity(Gravity.CENTER);
+                txtView.setTextSize(20);
+            }
+        });
 
+        final Button button2 = findViewById(R.id.simpleButton2);
+        button2.setText("Enter a custom date");
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                System.out.println("clicked");
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
-
         });
-
-        final Button button2 = (Button) findViewById(R.id.simpleButton2);
-        button2.setText("Enter a custom date");
-
-
-
-
     }
-
-
 }
